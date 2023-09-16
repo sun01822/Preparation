d049d7cc5101167990935390b587ec94b89eb32b
MDShariarHossainSun <mdshariarhossainsun01822@gmail.com>
2023-09-16 12:53:39 +0600

    Initial Commit

---
+++ app/src/main/java/com/sun/preparation/activities/SplashScreen.kt
@@ -0,0 +1,35 @@
+package com.sun.preparation.activities
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.bumptech.glide.Glide
+import com.sun.preparation.MainActivity
+import com.sun.preparation.R
+import com.sun.preparation.databinding.ActivitySplashScreenBinding
+
+@SuppressLint("CustomSplashScreen")
+class SplashScreen : AppCompatActivity() {
+    private lateinit var binding : ActivitySplashScreenBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Glide.with(this).load(R.drawable.brain).into(binding.imageView)
+        delayTimer()
+    }
+    private fun delayTimer() {
+        Handler(Looper.getMainLooper()).postDelayed({
+            goToNext()
+        },3500)
+    }
+    private fun goToNext() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+}
