e902309c15bab6856957775fd141e110bb83c992
MDShariarHossainSun <mdshariarhossainsun01822@gmail.com>
2023-09-19 12:54:36 +0600

    Login Backend added

--- app/src/main/java/com/sun/preparation/MainActivity.kt
+++ app/src/main/java/com/sun/preparation/MainActivity.kt
@@ -2,10 +2,20 @@ package com.sun.preparation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.sun.preparation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        // Retrieve the name from the Intent's extra
+        val name = intent.getStringExtra("name")
+
+        // Check if the name is not null and set it in a TextView
+        if (!name.isNullOrEmpty()) {
+            binding.textViewWelcome.text = "Welcome, $name!"
+        }
     }
 }
--- app/src/main/java/com/sun/preparation/activities/LoginActivity.kt
+++ app/src/main/java/com/sun/preparation/activities/LoginActivity.kt
@@ -1,7 +1,10 @@
 package com.sun.preparation.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.sun.preparation.MainActivity
 import com.sun.preparation.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
@@ -10,5 +13,22 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.btnLogin.setOnClickListener {
+            // Get the name from the EditText
+            val name = binding.editTextName.text.toString()
+
+            // Create an Intent to start MainActivity
+            if(name.isNotEmpty()){
+                val intent = Intent(this, MainActivity::class.java)
+                // Put the name as an extra in the Intent
+                intent.putExtra("name", name)
+                // Start MainActivity with the Intent
+                startActivity(intent)
+                finish()
+            }
+            else{
+                Toast.makeText(this, "Enter your name please!!!", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
