264a4eef033070576b34f02778d485306153ffca
MDShariarHossainSun <mdshariarhossainsun01822@gmail.com>
2023-10-17 11:32:06 +0600

    Bottom navigation added successfully

--- app/src/main/java/com/sun/preparation/activities/SlideActivity.kt
+++ app/src/main/java/com/sun/preparation/activities/SlideActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.WindowManager
 import android.widget.Button
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayout
