7de143172cf63bb2d04bf38cc7f55906b964171e
MDShariarHossainSun <mdshariarhossainsun01822@gmail.com>
2023-10-26 01:14:29 +0600

    Onclick added on Subjects

--- app/src/main/java/com/sun/preparation/fragments/HomeFragment.kt
+++ app/src/main/java/com/sun/preparation/fragments/HomeFragment.kt
@@ -1,18 +1,17 @@
 package com.sun.preparation.fragments
 
-import SubjectAdapter
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.bumptech.glide.Glide
 import com.sun.preparation.R
+import com.sun.preparation.adapter.SubjectAdapter
 import com.sun.preparation.data.Subject
 import com.sun.preparation.databinding.FragmentHomeBinding
-
+import com.sun.preparation.fragments.subjects.EnglishFragment
 
 class HomeFragment : Fragment() {
 
@@ -33,11 +32,27 @@ class HomeFragment : Fragment() {
 
         // Use view binding to get a reference to the RecyclerView
         // Setting the layout as Staggered Grid for vertical orientation
-        // Setting the layout as Staggered Grid for vertical orientation
         val subjectRecyclerView = binding?.subjectRecyclerView
-        val staggeredGridLayoutManager = StaggeredGridLayoutManager(2, LinearLayoutManager.VERTICAL)
+        val staggeredGridLayoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
         subjectRecyclerView?.layoutManager = staggeredGridLayoutManager
-        subjectRecyclerView?.adapter = SubjectAdapter(subjects)
+        val subjectAdapter = SubjectAdapter(subjects)
+        subjectRecyclerView?.adapter = subjectAdapter
+
+        // Set the item click listener for the adapter
+        subjectAdapter.setOnItemClickListener(object : SubjectAdapter.OnItemClickListener {
+            override fun onItemClick(subject: Subject) {
+                val transaction = requireActivity().supportFragmentManager.beginTransaction()
+                val newFragment = when (subject.name) {
+                    "English Language and Literature" -> EnglishFragment() // Open EnglishFragment for "English"
+                    else -> HomeFragment() // Open YourNextFragment for other titles
+                }
+
+                transaction.replace(R.id.fragmentContainer, newFragment)
+                transaction.addToBackStack(null)
+                transaction.commit()
+            }
+        })
+
 
         return rootView
     }
@@ -45,14 +60,14 @@ class HomeFragment : Fragment() {
     private fun generateSampleSubjects(): List<Subject> {
         val subjects = ArrayList<Subject>()
         subjects.add(Subject(1, "বাংলা ভাষা ও সাহিত্য", R.drawable.id1, 0.0))
-        subjects.add(Subject(2, "English Language and Literature", R.drawable.id2,  0.0))
+        subjects.add(Subject(2, "English Language and Literature", R.drawable.id2, 0.0))
         subjects.add(Subject(3, "বাংলাদেশ বিষয়াবলি", R.drawable.id3, 0.0))
         subjects.add(Subject(4, "আন্তর্জাতিক বিষয়াবলি", R.drawable.id4, 0.0))
-        subjects.add(Subject(5, "ভুগোল", R.drawable.id5,  0.0))
+        subjects.add(Subject(5, "ভুগোল", R.drawable.id5, 0.0))
         subjects.add(Subject(6, "পরিবেশ ও দূর্যোগ ব্যবস্থাপনা", R.drawable.id6, 0.0))
         subjects.add(Subject(7, "সাধারণ বিজ্ঞান", R.drawable.id7, 0.0))
-        subjects.add(Subject(8, "কম্পিউটার ও তথ্য প্রযুক্তি", R.drawable.id8,  0.0))
-        subjects.add(Subject(9, "গাণিতিক যুক্তি", R.drawable.id9,  0.0))
+        subjects.add(Subject(8, "কম্পিউটার ও তথ্য প্রযুক্তি", R.drawable.id8, 0.0))
+        subjects.add(Subject(9, "গাণিতিক যুক্তি", R.drawable.id9, 0.0))
         subjects.add(Subject(10, "মানসিক দক্ষতা", R.drawable.id10, 0.0))
         subjects.add(Subject(11, "নৈতিকতা, মূল্যবোধ ও সুশাসন", R.drawable.id11, 0.0))
         // Add more subjects as needed
